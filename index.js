9f0953267e38e1fa194cd2ab3be2654fc9d0208f
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-19 08:08:59 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function eventToListen(theEvent) {
+  alert('I was clicked!')
+  })
 }
+
